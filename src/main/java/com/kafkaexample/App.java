4c9a7e6404aacf19396bb2390cc1791e5eae64af
Jon Cameron <jon.cameron.yyz@gmail.com>
2024-03-01 11:37:31 -0500

    Cleanup and add some extra logging

--- src/main/java/com/kafkaexample/App.java
+++ src/main/java/com/kafkaexample/App.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -16,23 +17,30 @@ public class App {
   private static KafkaConsumer<String, String> consumer = null;
 
   private static void createProducer() {
+    System.out.println("Creating producer...");
     var properties = new Properties();
-    properties.put("bootstrap.servers", "localhost:9092");
-    properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092");
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
+
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        "org.apache.kafka.common.serialization.StringSerializer");
 
     producer = new KafkaProducer<>(properties);
+    System.out.println("Done!");
   }
 
   private static void createConsumer() {
+    System.out.println("Creating consumer...");
     var properties = new Properties();
-    properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:9092");
     properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group");
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
     consumer = new KafkaConsumer<>(properties);
+    System.out.println("Done!");
   }
 
   static {
@@ -58,9 +66,11 @@ public class App {
       System.out.println("Running...");
       dispatchMessage("my-topic", "Hello from Kafka!");
       System.out.println("Message dispatched.");
+
       var consumer = getConsumer();
       consumer.subscribe(Collections.singletonList("my-topic"));
-      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+      System.out.println("Polling for message...");
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
       for (var record : records) {
         System.out.println(record.value());
       }
